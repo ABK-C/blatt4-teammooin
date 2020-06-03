d53fe2172c25bc9dd01f230f62a891140d2a14b4
repl.it user <replituser@example.com>
2020-06-03 15:27:43 +0000

    Brainstorming Zwischenspeicher

--- zufall.cc
+++ zufall.cc
@@ -1,6 +1,7 @@
 
 #include<iostream>
 #include<cmath>
+#include<fstream>
 using namespace std;
 
 static int I = 2;
@@ -13,31 +14,45 @@ double zufall(int a, int c, int m) {
 
   int main () {
 
-    int a, c, m, n;
-    double Kreis = 0;
+    int a1, a2, c1, c2, m1, m2;
+    int n = 100;
+    double Kreis1 = 0;
+    double Kreis2 = 0;
+
     //Test1
-    //a = 5;
-    //c = 3;
-    //m = 16;
-    //n = 100;
+    a1 = 5;
+    c1 = 3;
+    m1 = 16;
    
     //Test2
-    //a = 205;
-    //c = 29573;
-    //m = 139968;
-    //n = 100;
+    //a2 = 205;
+    //c2 = 29573;
+    //m2 = 139968;
   
   for (int N = 0; N < n; ++N) {
-  if (pow(zufall(a, c, m), 2) + pow(zufall(a, c, m), 2) < 1) {
-    ++Kreis;
+  if (pow(zufall(a1, c1, m1), 2) + pow(zufall(a1, c1, m1), 2) < 1) {
+    ++Kreis1;
    }
   }
+  
+  //for (int N = 0; N < n; ++n) {
+    //if (pow(zufall(a2, c2, m2), 2) + pow(zufall(a2, c2, m2), 2) < 1) {
+      //++Kreis2;
+    //}
+  //}
 
     double p = M_PI / 4;
     double Ewert = p * n;
     double varianz = p * (1 - p) * n;
-    double pi = 4 * (Kreis / n);
+    double pi1 = 4 * (Kreis1 / n);
+    //double pi2 = 4 * (Kreis2 / n);
     double sigma = 4 * sqrt(varianz)/n;
+
+    ofstream fout("tabelle.txt");
+
+    fout << n << " " << Ewert << " " << varianz << " " << "|" << " " << Kreis1 << " " << pi1 << " " << sigma << endl;
+
+    fout.close();
 
     //Überprüfung für Test1 und Test2
     //cout << pi << endl;
